eab4b79c9a2abed5ea8898c82a3de143e2c46919
OscarOK <oscareom1a@gmail.com>
2017-11-13 18:37:58 -0700

    CHANGE ALL METHODS NAMES
    STILL WORK WITH REAL NUMBERS

--- vOperations.py
+++ vOperations.py
@@ -3,7 +3,7 @@ from math import acos, degrees #FROM MATH JUST IMPORT acos AND degrees
 #degrees: THE FUNCTIONS WORK ON RAD, THIS IS A WAY TO PASS RAD TO DEG
 
 def checkLen(vectorOne, vectorTwo):
-    
+
     if len(vectorOne) != len(vectorTwo): #CHECK IF THE VETORS ARE IN THE SAME SPACE
 
         print('MATCH ERROR: vectors in different Rn')
@@ -12,13 +12,6 @@ def checkLen(vectorOne, vectorTwo):
 
     return True
 
-def checkSpace(complex: vector):
-
-    if isinstance(vector, complex):
-        return True
-    else:
-        return False
-
 def checkSpace(vector):
 
     if isinstance(vector, complex):
@@ -26,38 +19,37 @@ def checkSpace(vector):
     else:
         return False
 
-def vectorInnerProduct(vectorOne, vectorTwo): #GET THE INNER PRODUCT OF VECTORS
+def vInnerProduct(vOne, vTwo): #GET THE INNER PRODUCT OF VECTORS
 
-    if len(vectorOne) != len(vectorTwo): #CHECK IF THE VETORS ARE IN THE SAME SPACE
+    if len(vOne) != len(vTwo): #CHECK IF THE VETORS ARE IN THE SAME SPACE
 
         print('MATCH ERROR: vectors in different Rn')
 
         return
 
-    if checkSpace(vectorOne) and checkSpace(vectorTwo):
 
-    rn = len(vectorOne) #GET THE LENGTH OF THE VECTOR
+    rn = len(vOne) #GET THE LENGTH OF THE VECTOR
     innerProduct = 0 #INIT RESULT VARIABLE
 
     for i in range(0, rn): #DO THIS rn TIMES
-        innerProduct += vectorOne[i] * vectorTwo[i]
+        innerProduct += vOne[i] * vTwo[i]
 
     return innerProduct
 
-def vectorNorm(vector): #GET THE NORM OF A VECTOR
+def vNorm(v): #GET THE NORM OF A VECTOR
 
-    return (vectorInnerProduct(vector, vector))**(1/2) #WE KNOW THAT THE NORM IS THE SQRT OF THE INNER PRODUCT OF THE VECTOR WITH ITSELF
+    return (vInnerProduct(v, v))**(1/2) #WE KNOW THAT THE NORM IS THE SQRT OF THE INNER PRODUCT OF THE VECTOR WITH ITSELF
 
-def vectorAngle(vectorOne, vectorTwo): #GET THE ANGLE BETWEEN TWO VECTORS
+def vAngle(vOne, vTwo): #GET THE ANGLE BETWEEN TWO VECTORS
 
-    if len(vectorOne) != len(vectorTwo):
+    if len(vOne) != len(vTwo):
 
         print('MATCH ERROR: vectors in different Rn')
 
         return
 
-    innerProduct = vectorInnerProduct(vectorOne, vectorTwo) #SET THE INNER PRODUCT BETWEEN THE TWO VECTORS
-    norms        = vectorNorm(vectorOne) * vectorNorm(vectorTwo) #SET THE PRODUCT BETWEEN THE NORM OF THE VECTORS
+    innerProduct = vInnerProduct(vOne, vTwo) #SET THE INNER PRODUCT BETWEEN THE TWO VECTORS
+    norms        = vNorm(vOne) * vNorm(vTwo) #SET THE PRODUCT BETWEEN THE NORM OF THE VECTORS
 
 
     if norms == 0: #JUST IN CASE WE ARE DEALING WITH A VECTOR ZERO
